b892c859cde356683c2fc76310e5196d0655e468
Thomas Pyle <thomas@pyle.tech>
2025-02-09 19:40:32 -0500

    Revert "Converts keyring load to pipe"
    This reverts commit 1db705197e7d91b0e0e27a9e267ac0c17399c70f.

--- lib/local_storage/keyring.go
+++ lib/local_storage/keyring.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/mitchellh/mapstructure"
 	"github.com/sirupsen/logrus"
@@ -41,43 +42,19 @@ func (fs *KeyringStorage) LoadConfig(config map[string]interface{}) error {
 }
 
 func (fs *KeyringStorage) Load() (io.Reader, error) {
-	// Create a pipe
-	pr, pw := io.Pipe()
-
-	// Channel to capture any errors from the goroutine
-	errChan := make(chan error, 1)
-
-	// Goroutine to read chunks from the keyring and write to the pipe writer
-	go func() {
-		defer pw.Close()
-
-		for i := 0; ; i++ {
-			val, err := keyring.Get(KeyringService, fmt.Sprintf("ksv_chunk_%d", i))
-			if err != nil {
-				if err == keyring.ErrNotFound {
-					break
-				}
-				errChan <- fmt.Errorf("error getting keyring value: %w", err)
-				return
+	var dataBuilder strings.Builder
+	for i := 0; ; i++ {
+		val, err := keyring.Get(KeyringService, fmt.Sprintf("ksv_chunk_%d", i))
+		if err != nil {
+			if err == keyring.ErrNotFound {
+				break
 			}
-			_, err = pw.Write([]byte(val))
-			if err != nil {
-				errChan <- fmt.Errorf("error writing to pipe: %w", err)
-				return
-			}
+			return nil, fmt.Errorf("error getting keyring value: %w", err)
 		}
-		errChan <- nil
-	}()
+		dataBuilder.WriteString(val)
+	}
 
-	// Wait for the goroutine to finish and check for errors
-	go func() {
-		if err := <-errChan; err != nil {
-			pw.CloseWithError(err)
-		}
-	}()
-
-	// Return a base64 decoder that reads from the pipe reader
-	return base64.NewDecoder(base64.StdEncoding, pr), nil
+	return base64.NewDecoder(base64.StdEncoding, strings.NewReader(dataBuilder.String())), nil
 }
 
 func (fs *KeyringStorage) Save(reader io.Reader) error {
