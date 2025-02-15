12f1d909189f9c4ecbaddd65b74cb16371733fe4
Thomas Pyle <thomas@pyle.tech>
2025-02-14 20:04:16 -0500

    Switches namespaces to be bytes in preparation for namespaces values

--- lib/types/ksv.go
+++ lib/types/ksv.go
@@ -1,19 +1,63 @@
 package types
 
+import (
+	"encoding/base64"
+	"encoding/json"
+)
+
 type KSV struct {
-	Namespaces map[string]Namespace `json:"namespaces"`
+	Namespaces map[string][]byte `json:"namespaces"`
 
 	// Default Namespace
 	DefaultNamespace Namespace `json:"default_namespace"`
 }
 
+func (k *KSV) UnmarshalJSON(data []byte) error {
+
+	aux := &struct {
+		Namespaces       map[string]string `json:"namespaces"`
+		DefaultNamespace Namespace         `json:"default_namespace"`
+	}{
+		Namespaces: make(map[string]string),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	k.Namespaces = make(map[string][]byte)
+	for key, val := range aux.Namespaces {
+		namespaceBytes, err := base64.StdEncoding.DecodeString(val)
+		if err != nil {
+			return err
+		}
+		k.Namespaces[key] = namespaceBytes
+	}
+
+	k.DefaultNamespace = aux.DefaultNamespace
+
+	return nil
+}
+
+func (k *KSV) MarshalJSON() ([]byte, error) {
+	aux := &struct {
+		Namespaces       map[string]string `json:"namespaces"`
+		DefaultNamespace Namespace         `json:"default_namespace"`
+	}{
+		Namespaces:       make(map[string]string),
+		DefaultNamespace: k.DefaultNamespace,
+	}
+
+	for key, val := range k.Namespaces {
+		aux.Namespaces[key] = base64.StdEncoding.EncodeToString(val)
+	}
+
+	return json.Marshal(aux)
+}
+
 func (k *KSV) Validate() []error {
 	var errs []error
 	errs = append(errs, k.DefaultNamespace.Validate()...)
-
-	for _, n := range k.Namespaces {
-		errs = append(errs, n.Validate()...)
-	}
 
 	return errs
 }
--- lib/types/ksv_test.go
+++ lib/types/ksv_test.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"encoding/json"
+	"reflect"
 	"testing"
 )
 
@@ -23,5 +25,29 @@ func TestKSV_Validate(t *testing.T) {
 	errs := ksv.Validate()
 	if len(errs) == 0 {
 		t.Errorf("Expected validation errors, got none")
+	}
+}
+
+func TestKSV_MarshalUnmarshalJSON(t *testing.T) {
+	ksv := KSV{
+		Namespaces: map[string][]byte{
+			"namespace1": []byte("data1"),
+			"namespace2": []byte("data2"),
+		},
+		DefaultNamespace: Namespace{},
+	}
+
+	data, err := json.Marshal(ksv)
+	if err != nil {
+		t.Fatalf("MarshalJSON failed: %v", err)
+	}
+
+	var unmarshaledKSV KSV
+	if err := json.Unmarshal(data, &unmarshaledKSV); err != nil {
+		t.Fatalf("UnmarshalJSON failed: %v", err)
+	}
+
+	if !reflect.DeepEqual(ksv, unmarshaledKSV) {
+		t.Errorf("Expected %v, got %v", ksv, unmarshaledKSV)
 	}
 }
